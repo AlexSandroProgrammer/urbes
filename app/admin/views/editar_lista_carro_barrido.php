f06fb26af86339e1b22ecdbc71a437ce37b594f9
alexsandro2005 <luisalejandrm533@gmail.com>
2024-09-25 06:11:14 -0500

    feat: mapeo de datos

--- app/admin/views/editar_lista_carro_barrido.php
+++ app/admin/views/editar_lista_carro_barrido.php
@@ -1,41 +1,22 @@
 <?php
 $titlePage = "Editar Carro de Barrido";
 require_once("../components/sidebar.php");
-
 // Obtener el ID del registro desde el parámetro GET
 $id_registro_barrido = $_GET['id_registro_barrido'];
-
 // Consulta para obtener los detalles del carro barrido seleccionado
-$queryBarrido = $connection->prepare("SELECT 
-        cb.*, 
-        c.ciudad AS ciudad_nombre,  -- Seleccionar el nombre de la ciudad
-        GROUP_CONCAT(dz.id_zona SEPARATOR '__') AS zonas_seleccionadas 
-    FROM 
-        carro_barrido AS cb
-    LEFT JOIN 
-        detalle_zonas AS dz ON cb.id_registro_barrido = dz.id_registro
-    LEFT JOIN 
-        ciudades AS c ON cb.ciudad = c.id_ciudad  -- Unir con la tabla de ciudades
-    WHERE 
-        cb.id_registro_barrido = :id_registro_barrido
-    GROUP BY 
-        cb.id_registro_barrido
-");
+$queryBarrido = $connection->prepare("SELECT cb.*, c.ciudad AS ciudad_nombre, GROUP_CONCAT(dz.id_zona SEPARATOR '__') AS zonas_seleccionadas FROM carro_barrido AS cb LEFT JOIN detalle_zonas AS dz ON cb.id_registro_barrido = dz.id_registro LEFT JOIN ciudades AS c ON cb.ciudad = c.id_ciudad WHERE cb.id_registro_barrido = :id_registro_barrido GROUP BY cb.id_registro_barrido");
 $queryBarrido->bindParam(':id_registro_barrido', $id_registro_barrido);
 $queryBarrido->execute();
 $carro_barrido = $queryBarrido->fetch(PDO::FETCH_ASSOC);
-
 // Obtener las zonas disponibles para esta ciudad
 $id_city = $carro_barrido['ciudad'];
 $queryZona = $connection->prepare("SELECT * FROM zonas WHERE id_ciudad = :id_ciudad");
 $queryZona->bindParam(":id_ciudad", $id_city);
 $queryZona->execute();
 $zonas = $queryZona->fetchAll(PDO::FETCH_ASSOC);
-
 // Separar las zonas seleccionadas
 $zonas_seleccionadas = explode('__', $carro_barrido['zonas_seleccionadas']);
 ?>
-
 <!-- Content wrapper -->
 <div class="content-wrapper">
     <div class="container-xxl flex-grow-1 container-p-y">
@@ -50,7 +31,6 @@ $zonas_seleccionadas = explode('__', $carro_barrido['zonas_seleccionadas']);
                     <div class="card-body">
                         <form method="POST" name="FormCarroBarrido" enctype="multipart/form-data" autocomplete="off">
                             <input type="hidden" name="id_registro_barrido" value="<?= $id_registro_barrido ?>">
-
                             <div class="row">
                                 <!-- Fecha Inicio -->
                                 <div class="mb-3 col-12 col-lg-6">
@@ -61,7 +41,6 @@ $zonas_seleccionadas = explode('__', $carro_barrido['zonas_seleccionadas']);
                                             value="<?= $carro_barrido['fecha_inicio'] ?>" required />
                                     </div>
                                 </div>
-
                                 <!-- Hora Inicio -->
                                 <div class="mb-3 col-12 col-lg-6">
                                     <label class="form-label" for="hora_inicio">Hora Inicio</label>
@@ -71,7 +50,6 @@ $zonas_seleccionadas = explode('__', $carro_barrido['zonas_seleccionadas']);
                                             value="<?= $carro_barrido['hora_inicio'] ?>" required />
                                     </div>
                                 </div>
-
                                 <!-- Fecha Fin -->
                                 <div class="mb-3 col-12 col-lg-6">
                                     <label class="form-label" for="fecha_fin">Fecha Fin</label>
@@ -81,7 +59,6 @@ $zonas_seleccionadas = explode('__', $carro_barrido['zonas_seleccionadas']);
                                             value="<?= $carro_barrido['fecha_fin'] ?>" required />
                                     </div>
                                 </div>
-
                                 <!-- Hora Fin -->
                                 <div class="mb-3 col-12 col-lg-6">
                                     <label class="form-label" for="hora_fin">Hora Fin</label>
@@ -91,7 +68,6 @@ $zonas_seleccionadas = explode('__', $carro_barrido['zonas_seleccionadas']);
                                             value="<?= $carro_barrido['hora_fin'] ?>" required />
                                     </div>
                                 </div>
-
                                 <!-- Ciudad -->
                                 <div class="mb-3 col-12 col-lg-6">
                                     <label class="form-label" for="ciudad">Ciudad</label>
@@ -99,11 +75,8 @@ $zonas_seleccionadas = explode('__', $carro_barrido['zonas_seleccionadas']);
                                         <option value="<?= $carro_barrido['ciudad'] ?>">
                                             <?= $carro_barrido['ciudad_nombre'] ?>
                                         </option>
-
-
                                     </select>
                                 </div>
-
                                 <!-- Zonas -->
                                 <div class="mb-3 col-12">
                                     <label class="form-label">Selecciona las Zonas:</label>
@@ -123,7 +96,6 @@ $zonas_seleccionadas = explode('__', $carro_barrido['zonas_seleccionadas']);
                                         <?php endforeach; ?>
                                     </div>
                                 </div>
-
                                 <!-- Peso -->
                                 <div class="mb-3 col-12 col-lg-6">
                                     <label class="form-label" for="peso">Peso</label>
@@ -134,7 +106,6 @@ $zonas_seleccionadas = explode('__', $carro_barrido['zonas_seleccionadas']);
                                             required />
                                     </div>
                                 </div>
-
                                 <!-- Observaciones -->
                                 <div class="mb-3 col-12">
                                     <label class="form-label" for="observaciones">Observaciones</label>
